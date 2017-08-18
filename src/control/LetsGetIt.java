05f1be77d67520ad1ed203f50dfebf90eb529096
hanajin <hanajin110@gmail.com>
2017-08-18 19:52:02 +0900

    Basic Classes

--- src/control/LetsGetIt.java
+++ src/control/LetsGetIt.java
@@ -1,10 +1,25 @@
 package control;
 
-public class LetsGetIt {
+import javax.swing.*;
 
+import java.awt.*;
+import java.awt.event.*;
+
+public class LetsGetIt extends JFrame /*implements ActionListener */{
+
+	
+	public LetsGetIt() {
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		MusicPlay musicPlay = new MusicPlay();			// 김대호, 유혜미 구현
+		Chatting chatting = new Chatting();				// 한상철 구현
+		Help help = new Help();							// 윤재혁 구현
+		LetsGetIt letsGetIt = new LetsGetIt();			// 공통 구현
+		
+		
 	}
 
 }
