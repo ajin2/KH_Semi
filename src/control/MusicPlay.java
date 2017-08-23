e11fa6b1c339d4bc1a61fe0198bc33baa5856c50
hyemi <dutjz26@gmail.com>
2017-08-23 16:38:21 +0900

    MusicPlayClient add

--- src/control/MusicPlay.java
+++ src/control/MusicPlay.java
@@ -6,79 +6,77 @@ import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
 
-public class MusicPlay extends JFrame{
+public class MusicPlay extends JFrame {
 	SheetMusic sm;
 	int index;
 	Image img = null, chimg;
 	JMenuBar mb;
 	JMenu mdrawing;
-	JMenuItem msheet,mmemo,msave;
-	JPanel psheet,pmusic;
+	JMenuItem msheet, mmemo, msave;
+	JPanel psheet, pmusic;
 	JButton btn;
-	
-	
-	public void init(){
+
+	public void init() {
 		psheet = new JPanel();
-		psheet.setLayout(new GridLayout(1,1));
+		psheet.setLayout(new GridLayout(1, 1));
 		chimg = img.getScaledInstance(650, 420, java.awt.Image.SCALE_SMOOTH);
-	    JLabel sheet = new JLabel(new ImageIcon(chimg));
-	    psheet.add(sheet);
-	   }
-	
-	public void addGrid(GridBagLayout gbl, GridBagConstraints gbc, Component c,  
-            int gridx, int gridy, int gridwidth, int gridheight, int weightx, int weighty) {
-      gbc.gridx = gridx;
-      gbc.gridy = gridy;
-      gbc.gridwidth = gridwidth;
-      gbc.gridheight = gridheight;
-      gbc.weightx = weightx;
-      gbc.weighty = weighty;
-      gbl.setConstraints(c, gbc);
-      add(c);
+		JLabel sheet = new JLabel(new ImageIcon(chimg));
+		psheet.add(sheet);
 	}
-	
-	public MusicPlay(int index) throws IOException{
-		
+
+	public void addGrid(GridBagLayout gbl, GridBagConstraints gbc, Component c, int gridx, int gridy, int gridwidth,
+			int gridheight, int weightx, int weighty) {
+		gbc.gridx = gridx;
+		gbc.gridy = gridy;
+		gbc.gridwidth = gridwidth;
+		gbc.gridheight = gridheight;
+		gbc.weightx = weightx;
+		gbc.weighty = weighty;
+		gbl.setConstraints(c, gbc);
+		add(c);
+	}
+
+	public MusicPlay(int index) throws IOException {
+
 		// MenuBar
 		mb = new JMenuBar();
 		setJMenuBar(mb);
-			
+
 		mdrawing = new JMenu("Drawing");
 		mb.add(mdrawing);
-			
+
 		msheet = new JMenuItem("On Sheet");
 		mmemo = new JMenuItem("New Memo");
 		msave = new JMenuItem("Save");
-			
+
 		mdrawing.add(msheet);
 		mdrawing.add(mmemo);
 		mdrawing.add(msave);
-		
+
 		// Sheet Print
-		
+
 		sm = new SheetMusic();
 		img = ImageIO.read(sm.showSheet(index));
 		init();
-		//----------------------------------------------- ¾Çº¸Ãâ·Â
-		
-		btn = new JButton("¾Ç±â");
-		//----------------------------------------------- ¾Ç±â±¸Çö Ãâ·Â
-		
+		// ----------------------------------------------- ì•…ë³´
+
+		btn = new JButton("ì•…ê¸°");
+		// ----------------------------------------------- ì•…ê¸°êµ¬í˜„
+
 		GridBagLayout gbl = new GridBagLayout();
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
-        setLayout(gbl);
-         
-        addGrid(gbl, gbc, psheet, 0, 0, 1, 1, 1, 1);
-        addGrid(gbl, gbc, btn, 	  0, 1, 1, 1, 1, 4);
-         
-        pack();
-		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		setLayout(gbl);
+
+		addGrid(gbl, gbc, psheet, 0, 0, 1, 1, 1, 1);
+		addGrid(gbl, gbc, btn, 0, 1, 1, 1, 1, 4);
+
+		pack();
+
 		setVisible(true);
-	    setBounds(118,50,1237,893);
-	    setAlwaysOnTop(true);
-	    setResizable(false);
-		
+		setBounds(118, 50, 1237, 893);
+		setAlwaysOnTop(true);
+		setResizable(false);
+
 	}
 }
- 
