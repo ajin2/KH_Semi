b6b59fb307bfc4af620efdf46e13e06851f1e256
hyemi <dutjz26@gmail.com>
2017-08-23 15:30:29 +0900

    채팅창 수정

--- src/control/LetsGetIt.java
+++ src/control/LetsGetIt.java
@@ -8,16 +8,22 @@ import java.io.IOException;
 
 public class LetsGetIt extends JFrame /*implements ActionListener */{
 	LetsGetItServer letsGetItS;
+	LetsGetItClient letsGetItC;
 	
-	private void open() {
+	private void Server() {
 		//악보 선택 시 윈도우 창에 악보 띄워줌
 		letsGetItS = new LetsGetItServer();
+	}
+	
+	private void Client(){
+		letsGetItC = new LetsGetItClient();
 	}
 	
 	public static void main(String[] args) throws IOException {
 		
 		LetsGetIt letsGetIt = new LetsGetIt();			
-		letsGetIt.open();							
+		letsGetIt.Server();			
+		//letsGetIt.Client();
 	}
 
 }
--- src/control/LetsGetItClient.java
+++ src/control/LetsGetItClient.java
@@ -1,71 +1,195 @@
 package control;
 
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
 
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
-public class LetsGetItClient extends JFrame /*implements ActionListener */{
-	private JPanel p,p1,p2,p3,pall;
-	private JButton ok, chat,btn1,btn2,btn3,btn4;
-	private JLabel sheet_la,blank;
-	private JComboBox sheet;
-	private String sheet_name[] = {"곰세마리","나비야","학교종이 땡땡땡","버즈-겁쟁이","메모"};
-	
-	public LetsGetItClient() {
-		// 악보
-		p = new JPanel();
-		p.setLayout(new GridLayout(1,4,5,5));
-		p1 = new JPanel();
+public class LetsGetItClient extends JFrame implements ActionListener {
+   Insets insets = new Insets(0, 0, 0, 0);
+   MusicPlay mp;
+   SheetMusic sm;
+   int index;
+   JPanel p,p1,p2,p3,p4,p5,p6,p_;
+   JButton ok,send,btn1,btn2,btn3,btn4,b1,b2;
+   JTextArea ta;
+   JTextField tf;
+   JScrollPane jsp;
+   JLabel sheet_la,blank;
+   JComboBox <String> sheet;
+   String sheet_name[] = {"곰세마리","나비야","학교종이 땡땡땡","버즈-겁쟁이","메모"};
+   
+   public int getIndex(){
+      index = sheet.getSelectedIndex();
+      return index;
+   }
+   
+   public void addGrid(GridBagLayout gbl, GridBagConstraints gbc, Component c,  
+            int gridx, int gridy, int gridwidth, int gridheight, double weightx, int weighty) {
+      gbc.gridx = gridx;
+      gbc.gridy = gridy;
+      gbc.gridwidth = gridwidth;
+      gbc.gridheight = gridheight;
+      gbc.weightx = weightx;
+      gbc.weighty = weighty;
+      gbl.setConstraints(c, gbc);
+      add(c);
+   }
+   
+   public LetsGetItClient() {
+      
+      // GridBagLayout
+      // SheetName, Button
+	  p_ = new JPanel();
+	  p_.setBackground(new Color(246,246,246));
+	  
+	  blank = new JLabel();
+	  
+	  p_.add(blank);
+	   
+      p = new JPanel();
+      p.setBackground(new Color(246,246,246));
+      p.setLayout(new GridLayout(1,4,5,5));
+      p1 = new JPanel();
+      p1.setBackground(new Color(246,246,246));
 
-		blank = new JLabel();
-		sheet_la = new JLabel("악보 : ");
-		sheet = new JComboBox(sheet_name);
-		ok = new JButton("선택");
-				
-		p1.add(sheet_la);
-		p1.add(sheet);
-		p1.add(ok);
+      sheet_la = new JLabel("악보 : ");
+      sheet = new JComboBox <String> (sheet_name);
+      ok = new JButton("선택");
+      ok.setEnabled(false);
+               
+      p1.add(sheet_la);
+      p1.add(sheet);
+      p1.add(ok);
 
-		p.add(p1);
-		p.add(blank);
-		p.add(blank);
-		p.add(blank);
-		add(p,"North");
-				
-		// 악기
-		p2 = new JPanel();
-		p2.setLayout(new GridLayout(2,2,10,10));
-			
-		btn1 = new JButton();
-		btn2 = new JButton();
-		btn3 = new JButton();
-		btn4 = new JButton();
-				
-		p2.add(btn1);
-		p2.add(btn2);
-		p2.add(btn3);
-		p2.add(btn4);
-		add(p2);
+      p.add(p1);
+      
+      // Chatting
+      
+      p2 = new JPanel();
+      p3 = new JPanel();
+      p4 = new JPanel();
+      p5 = new JPanel();
+      p6 = new JPanel();
+      
+      p2.setLayout(new BorderLayout());
+      p3.setLayout(new BorderLayout());
+      p3.setBackground(new Color(246,246,246));
+      p4.setLayout(new BorderLayout());
+      p5.setLayout(new BorderLayout());
+      p5.setBackground(new Color(246,246,246));
+      p6.setLayout(new BorderLayout());
+      
+      Font font = new Font("Courier", Font.PLAIN, 23);
+      
+      ta = new JTextArea("",30,20);
+      ta.setFont(font);
+      
+        tf = new JTextField(20);
+        tf.setFont(font);
+        
+        send = new JButton("입력");
+        b1 = new JButton("파일선택");
+        b2 = new JButton("파일전송");
+        
+        p4.add("West", tf);
+        p4.add("East",send);
+        p2.add("South",p4);
+       
+        p5.add("West",b1);
+        p5.add("East",b2);
+        p3.add("West", p5);
+        p2.add("North",p3);
+        
+        ta.setLineWrap(true);
+       jsp = new JScrollPane(ta);
+       jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+       jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED); 
+       
+        p6.add(jsp);
+        p2.add(p6);
+        
+        // p3 > West > p5 > North > b1,b2
+        // p4 > South > tf,send
+        // p6 > Center > ta ----------------------------------------------------------
+        
+         btn1 = new JButton("Piano");
+         btn2 = new JButton("Electric");
+         btn3 = new JButton("Bass");
+         btn4 = new JButton("Drum");
+         
+         
+         GridBagLayout gbl = new GridBagLayout();
+         GridBagConstraints gbc = new GridBagConstraints();
+         gbc.fill = GridBagConstraints.BOTH;
+         setLayout(gbl);
+         
+         addGrid(gbl, gbc, p,    0, 0, 4, 1, 0.5, 0);
+         addGrid(gbl, gbc, p_, 	 5, 0, 1, 1, 0.5, 0);
+         addGrid(gbl, gbc, btn1, 0, 1, 2, 2, 1, 2);
+         addGrid(gbl, gbc, btn2, 2, 1, 2, 2, 1, 2);
+         addGrid(gbl, gbc, btn3, 0, 4, 2, 2, 1, 2);
+         addGrid(gbl, gbc, btn4, 2, 4, 2, 2, 1, 2);
+         addGrid(gbl, gbc, p2,   5, 1, 1, 5, 0.5, 5);
+         
+         pack();
+            
+      setVisible(true);
+      setBounds(115,50,1670,900);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //key event
+      
+      tf.addKeyListener(
+               new KeyAdapter() {
+               public void keyPressed( KeyEvent e) {
+                  switch( e.getKeyCode() ) {
+                  case KeyEvent.VK_ESCAPE :
+                     tf.setText( "" );
+                     break;
+                  }
 
-		// 채팅
-		p3 = new JPanel();
-		p3.setLayout(new GridLayout(1,1));
-
-		chat = new JButton("채팅");
-		p3.add(chat);
-
-		// 악기 + 채팅
-		pall = new JPanel();
-		pall.setLayout(new GridLayout(1,2,10,10));
-				
-		pall.add(p2);
-		pall.add(p3);
-		add(pall);
-				
-				
-		setVisible(true);
-		setBounds(100,50,1400,900);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+                  }
+               }
+            );
+      ok.addActionListener(this);
+      tf.addActionListener( this );
+      b1.addActionListener( this );
+      b2.addActionListener( this );
+      send.addActionListener(this);
+      
+   }
+   //action Event
+   public void actionPerformed(ActionEvent e){
+      Object obj = e.getSource();
+      String msg = tf.getText();
+      
+      if(obj == send || obj ==tf) {
+         String str = tf.getText();
+         if(!str.equals("")) {
+            ta.append(" >> "+str+"\n");
+            tf.setText("");
+         }
+      }
+   }
 }
