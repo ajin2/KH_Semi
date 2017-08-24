bcdf7c5e23030387dbc12eaebc33c10a93032a9f
hanajin <hanajin110@gmail.com>
2017-08-25 07:49:12 +0900

    LogIn & Network

---
+++ src/control/Ex1.java
@@ -0,0 +1,64 @@
+package control;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.lang.*;
+import java.net.UnknownHostException;
+import java.util.*;
+import javax.swing.*;
+
+class Ex1 extends JFrame implements ActionListener {
+	JPanel Hyeok;
+	JLabel la;
+	JTextField tf;
+	String id;
+	JButton loginButton;
+	String str;
+	LetsGetItClient lc;
+	
+	public Ex1() throws UnknownHostException, IOException {
+		Hyeok = new JPanel();
+		la = new JLabel(" 채팅닉네임 : ");
+		tf = new JTextField(15);
+		loginButton = new JButton("Login");
+		Hyeok.add(loginButton);
+		Hyeok.add(la);
+		Hyeok.add(tf);
+		
+		setBounds(500, 50, 250, 250);
+		setVisible(true);
+		add(Hyeok);
+		
+		loginButton.addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		Object obj = e.getSource();
+
+		if (obj == loginButton) {
+			str = tf.getText();
+			setStr(str);
+			
+			LetsGetItClient letsGetItC;
+			try {
+				letsGetItC = new LetsGetItClient("localhost", 4500, getStr());
+				letsGetItC.init();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		new Ex1();
+	}
+}
